1d6b547d441de584edf5cc7cd560b5ecd72d2daf
Fernando Funez <ferfunezdev@gmail.com>
2020-06-01 23:21:46 +0000

    updates

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -31,7 +31,7 @@ export class Home extends React.Component {
 				<div
 					style={{ color: "white", backgroundColor: "red" }}
 					className="ui container">
-					ERROOOOR! {errorMessage}
+					ERROOR! {errorMessage}
 				</div>
 			);
 		} else if (latitude && !errorMessage) {
